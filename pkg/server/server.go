40b41850b5809fb262496f6984b2b8411720399c
tachunwu <tachunwu@gapp.nthu.edu.tw>
2022-04-13 15:12:20 +0800

    add validator

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"context"
+	"errors"
 	"log"
 
 	corev1 "github.com/tachunwu/fractal/pkg/proto/core/v1"
@@ -13,21 +14,41 @@ type FractalServiceServer struct {
 
 func (s *FractalServiceServer) ListUsers(ctx context.Context, req *corev1.ListUsersRequest) (*corev1.ListUsersResponse, error) {
 	log.Println(req)
+	err := req.Validate()
+	if err != nil {
+		return nil, errors.New("request format error")
+	}
 	return &corev1.ListUsersResponse{}, nil
 }
 func (s *FractalServiceServer) CreateUser(ctx context.Context, req *corev1.CreateUserRequest) (*corev1.CreateUserResponse, error) {
 	log.Println(req)
+	err := req.Validate()
+	if err != nil {
+		return nil, errors.New("request format error")
+	}
 	return &corev1.CreateUserResponse{}, nil
 }
 func (s *FractalServiceServer) GetUser(ctx context.Context, req *corev1.GetUserRequest) (*corev1.GetUserResponse, error) {
 	log.Println(req)
+	err := req.Validate()
+	if err != nil {
+		return nil, errors.New("request format error")
+	}
 	return &corev1.GetUserResponse{}, nil
 }
 func (s *FractalServiceServer) UpdateUser(ctx context.Context, req *corev1.UpdateUserRequest) (*corev1.UpdateUserResponse, error) {
 	log.Println(req)
+	err := req.Validate()
+	if err != nil {
+		return nil, errors.New("request format error")
+	}
 	return &corev1.UpdateUserResponse{}, nil
 }
 func (s *FractalServiceServer) DeleteUser(ctx context.Context, req *corev1.DeleteUserRequest) (*corev1.DeleteUserResponse, error) {
 	log.Println(req)
+	err := req.Validate()
+	if err != nil {
+		return nil, errors.New("request format error")
+	}
 	return &corev1.DeleteUserResponse{}, nil
 }
